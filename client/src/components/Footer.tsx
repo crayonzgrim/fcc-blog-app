868414495535c07a15de4e035307aaa1da0cb35e
KIM DONG IL <crayonzgrim@gmail.com>
2024-03-23 22:42:18 +0900

    :hammer: Footer component
    Footer component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,10 @@
+import Logo from '../assets/logo.png';
+
 export const Footer = () => {
-  return <>Footer</>;
+  return (
+    <footer>
+      <img src={Logo} alt="logo" />
+      <span>Made with my love</span>
+    </footer>
+  );
 };
