f813fa60bc276c81a0b380e44fd698e70a3047b1
KIM DONG IL <crayonzgrim@gmail.com>
2024-03-23 23:22:41 +0900

    :hammer:Write component
    Client page is done

--- client/src/pages/Write.tsx
+++ client/src/pages/Write.tsx
@@ -1,3 +1,116 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 export const Write = () => {
-  return <>Write</>;
+  const [quillValue, setQuillValue] = useState('');
+  const [title, setTitle] = useState('');
+
+  return (
+    <div className="add">
+      <div className="content">
+        <input
+          type="text"
+          placeholder="Title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={quillValue}
+            onChange={setQuillValue}
+          />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input
+            style={{ display: 'none' }}
+            type="file"
+            id="file"
+            name="file"
+            onChange={() => {}}
+          />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button onClick={() => {}}>Publish</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div className="cat">
+            <input
+              type="radio"
+              name="cat"
+              value="art"
+              id="art"
+              onChange={() => {}}
+            />
+            <label htmlFor="art">Art</label>
+          </div>
+          <div className="cat">
+            <input
+              type="radio"
+              name="cat"
+              value="science"
+              id="science"
+              onChange={() => {}}
+            />
+            <label htmlFor="science">Science</label>
+          </div>
+          <div className="cat">
+            <input
+              type="radio"
+              name="cat"
+              value="technology"
+              id="technology"
+              onChange={() => {}}
+            />
+            <label htmlFor="technology">Technology</label>
+          </div>
+          <div className="cat">
+            <input
+              type="radio"
+              name="cat"
+              value="cinema"
+              id="cinema"
+              onChange={() => {}}
+            />
+            <label htmlFor="cinema">Cinema</label>
+          </div>
+          <div className="cat">
+            <input
+              type="radio"
+              name="cat"
+              value="design"
+              id="design"
+              onChange={() => {}}
+            />
+            <label htmlFor="design">Design</label>
+          </div>
+          <div className="cat">
+            <input
+              type="radio"
+              name="cat"
+              value="food"
+              id="food"
+              onChange={() => {}}
+            />
+            <label htmlFor="food">Food</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
