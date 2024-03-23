d9056dc178f1e1bd9167e219d879d29907566dc0
KIM DONG IL <crayonzgrim@gmail.com>
2024-03-23 18:47:11 +0900

    :hammer: Register page
    Login page modify and Register page update

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,11 +1,28 @@
+import { Link } from 'react-router-dom';
+
 export const Login = () => {
   return (
     <div className="auth">
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <button>Login</button>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={() => {}}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={() => {}}
+        />
+        <button onClick={() => {}}>Login</button>
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
       </form>
     </div>
   );
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,3 +1,36 @@
+import { Link } from 'react-router-dom';
+
 export const Register = () => {
-  return <>Register</>;
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={() => {}}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={() => {}}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={() => {}}
+        />
+        <button onClick={() => {}}>Register</button>
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
